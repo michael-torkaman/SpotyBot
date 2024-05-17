1a64581f786aa44513f8c3ae39ba1942ed456835
michael-torkaman <michael.torkaman.t@gmail.com>
2024-05-17 14:09:58 -0700

    created StartBotAsync(). ignore .env for token

---
+++ SpotyBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.WebSocket;
+namespace SpotyBot{
+    internal class Program{
+        private readonly  DiscordSocketClient client; 
+
+
+        private const string token = "";
+        
+        public Program()
+        {
+            this.client = new DiscordSocketClient();
+        }
+
+
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+        }
+        
+        public async Task StartBotAsync(){
+        }
+
+    }
+}
+
